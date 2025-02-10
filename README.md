96f01c1f99122ce487481ca77d5f4e9b41377a9e
lihaiming <1353508760@qq.com>
2025-02-10 21:45:34 +0800

    第一章全部提交

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# 01-rcli s
+# 01-rcli
+
+## 第一讲使用到的工具:
+### 1. clap crate基本用法来构建rust命令行工具
+### 2. 使用duckdb 查看 csv数据
+### 3. csv crate 基本用法 在rust中读取csv文件
+### 4. 使用serde来进行序列化和反序列化
+### 5. 使用serde-json 把数据序列化成json
+### 6. 使用tokei 工具进行所有文件的解析统计
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+// rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+use rcli::{process_csv, Opts, Subcommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        Subcommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
